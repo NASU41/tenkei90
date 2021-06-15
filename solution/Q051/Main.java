734d4c18150fca4b70c37a375ac38eec0ffbacb0
NASU41 <sakon.n68@gmail.com>
2021-06-15 12:48:03 +0900

    solved Q051

---
+++ solution/Q051/Main.java
@@ -0,0 +1,362 @@
+import java.util.*;
+
+public class Main {
+    static<T extends Comparable<T>> int lowerBound(List<T> list, T elm){
+        int N = list.size();
+        int l = -1, r = N;
+        while(r-l > 1){
+            int mid = (r+l)/2;
+            if(elm.compareTo(list.get(mid)) <= 0) r = mid;
+            else l = mid;
+        }
+        return r;
+    }
+    static<T extends Comparable<T>> int upperBound(List<T> list, T elm){
+        int N = list.size();
+        int l = -1, r = N;
+        while(r-l > 1){
+            int mid = (r+l)/2;
+            if(elm.compareTo(list.get(mid)) < 0) r = mid;
+            else l = mid;
+        }
+        return r;
+    }
+    
+    private static ArrayList<Long>[] bruteforce(List<Long> list){
+        int N = list.size();
+        ArrayList<Long>[] ans = new ArrayList[N+1];
+        for(int n=0; n<=N; n++) ans[n] = new ArrayList<>();
+        ans[0].add(0L);
+
+        for(Long item: list){
+            for(int n=N-1; n>=0; n--){
+                for(long prev: ans[n]) ans[n+1].add(prev+item);
+            }
+        }
+        for(int n=0; n<=N; n++) Collections.sort(ans[n]);
+        return ans;
+    }
+    public static void main(String[] args) throws Exception{
+        final ContestScanner sc = new ContestScanner();
+        final ContestPrinter pr = new ContestPrinter();
+
+        int N = sc.nextInt(), K = sc.nextInt();
+        long P = sc.nextLong();
+        
+        long[] A = sc.nextLongArray(N);
+
+        List<Long> oddItems = new ArrayList<>(), evenItems = new ArrayList<>();
+        for(int n=0; n<N; n++){
+            if(n%2==0) evenItems.add(A[n]);
+            else oddItems.add(A[n]);
+        }
+
+        long ans = 0;
+
+        ArrayList<Long>[] oddPattern = bruteforce(oddItems), evenPattern = bruteforce(evenItems);
+
+        for(int o=0; o<=K; o++){
+            int e = K - o;
+            if(o >= oddPattern.length || e >= evenPattern.length) continue;
+            for(long oddPrice: oddPattern[o]){
+                ans += upperBound(evenPattern[e], P-oddPrice);
+            }
+        }
+        
+        pr.println(ans);
+        
+        pr.close();
+    }
+}
+
+class ContestScanner {
+    private final java.io.InputStream in;
+    private final byte[] buffer = new byte[1024];
+    private int ptr = 0;
+    private int buflen = 0;
+
+    private static final long LONG_MAX_TENTHS = 922337203685477580L;
+    private static final int LONG_MAX_LAST_DIGIT = 7;
+    private static final int LONG_MIN_LAST_DIGIT = 8;
+
+    public ContestScanner(java.io.InputStream in){
+        this.in = in;
+    }
+    public ContestScanner(){
+        this(System.in);
+    }
+ 
+    private boolean hasNextByte() {
+        if (ptr < buflen) {
+            return true;
+        }else{
+            ptr = 0;
+            try {
+                buflen = in.read(buffer);
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
+            if (buflen <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private int readByte() { 
+        if (hasNextByte()) return buffer[ptr++]; else return -1;
+    }
+    private static boolean isPrintableChar(int c) {
+        return 33 <= c && c <= 126;
+    }
+    public boolean hasNext() {
+        while(hasNextByte() && !isPrintableChar(buffer[ptr])) ptr++;
+        return hasNextByte();
+    }
+    public String next() {
+        if (!hasNext()) throw new java.util.NoSuchElementException();
+        StringBuilder sb = new StringBuilder();
+        int b = readByte();
+        while(isPrintableChar(b)) {
+            sb.appendCodePoint(b);
+            b = readByte();
+        }
+        return sb.toString();
+    }
+ 
+    public long nextLong() {
+        if (!hasNext()) throw new java.util.NoSuchElementException();
+        long n = 0;
+        boolean minus = false;
+        int b = readByte();
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+        if (b < '0' || '9' < b) {
+            throw new NumberFormatException();
+        }
+        while (true) {
+            if ('0' <= b && b <= '9') {
+                int digit = b - '0';
+                if (n >= LONG_MAX_TENTHS) {
+                    if (n == LONG_MAX_TENTHS) {
+                        if (minus) {
+                            if (digit <= LONG_MIN_LAST_DIGIT) {
+                                n = -n * 10 - digit;
+                                b = readByte();
+                                if (!isPrintableChar(b)) {
+                                    return n;
+                                } else if (b < '0' || '9' < b) {
+                                    throw new NumberFormatException(
+                                        String.format("%d%s... is not number", n, Character.toString(b))
+                                    );
+                                }
+                            }
+                        } else {
+                            if (digit <= LONG_MAX_LAST_DIGIT) {
+                                n = n * 10 + digit;
+                                b = readByte();
+                                if (!isPrintableChar(b)) {
+                                    return n;
+                                } else if (b < '0' || '9' < b) {
+                                    throw new NumberFormatException(
+                                        String.format("%d%s... is not number", n, Character.toString(b))
+                                    );
+                                }
+                            }
+                        }
+                    }
+                    throw new ArithmeticException(
+                        String.format("%s%d%d... overflows long.", minus ? "-" : "", n, digit)
+                    );
+                }
+                n = n * 10 + digit;
+            }else if(b == -1 || !isPrintableChar(b)){
+                return minus ? -n : n;
+            }else{
+                throw new NumberFormatException();
+            }
+            b = readByte();
+        }
+    }
+    public int nextInt() {
+        long nl = nextLong();
+        if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE) throw new NumberFormatException();
+        return (int) nl;
+    }
+    public double nextDouble() {
+        return Double.parseDouble(next());
+    }
+ 
+    public long[] nextLongArray(int length){
+        long[] array = new long[length];
+        for(int i=0; i<length; i++) array[i] = this.nextLong();
+        return array;
+    }
+    public long[] nextLongArray(int length, java.util.function.LongUnaryOperator map){
+        long[] array = new long[length];
+        for(int i=0; i<length; i++) array[i] = map.applyAsLong(this.nextLong());
+        return array;
+    }
+    public int[] nextIntArray(int length){
+        int[] array = new int[length];
+        for(int i=0; i<length; i++) array[i] = this.nextInt();
+        return array;
+    }
+    public int[] nextIntArray(int length, java.util.function.IntUnaryOperator map){
+        int[] array = new int[length];
+        for(int i=0; i<length; i++) array[i] = map.applyAsInt(this.nextInt());
+        return array;
+    }
+    public double[] nextDoubleArray(int length){
+        double[] array = new double[length];
+        for(int i=0; i<length; i++) array[i] = this.nextDouble();
+        return array;
+    }
+    public double[] nextDoubleArray(int length, java.util.function.DoubleUnaryOperator map){
+        double[] array = new double[length];
+        for(int i=0; i<length; i++) array[i] = map.applyAsDouble(this.nextDouble());
+        return array;
+    }
+ 
+    public long[][] nextLongMatrix(int height, int width){
+        long[][] mat = new long[height][width];
+        for(int h=0; h<height; h++) for(int w=0; w<width; w++){
+            mat[h][w] = this.nextLong();
+        }
+        return mat;
+    }
+    public int[][] nextIntMatrix(int height, int width){
+        int[][] mat = new int[height][width];
+        for(int h=0; h<height; h++) for(int w=0; w<width; w++){
+            mat[h][w] = this.nextInt();
+        }
+        return mat;
+    }
+    public double[][] nextDoubleMatrix(int height, int width){
+        double[][] mat = new double[height][width];
+        for(int h=0; h<height; h++) for(int w=0; w<width; w++){
+            mat[h][w] = this.nextDouble();
+        }
+        return mat;
+    }
+ 
+    public char[][] nextCharMatrix(int height, int width){
+        char[][] mat = new char[height][width];
+        for(int h=0; h<height; h++){
+            String s = this.next();
+            for(int w=0; w<width; w++){
+                mat[h][w] = s.charAt(w);
+            }
+        }
+        return mat;
+    }
+}
+
+class ContestPrinter extends java.io.PrintWriter{
+    public ContestPrinter(java.io.PrintStream stream){
+        super(stream);
+    }
+    public ContestPrinter(){
+        super(System.out);
+    }
+    
+    private static String dtos(double x, int n) {
+        StringBuilder sb = new StringBuilder();
+        if(x < 0){
+            sb.append('-');
+            x = -x;
+        }
+        x += Math.pow(10, -n)/2;
+        sb.append((long)x);
+        sb.append(".");
+        x -= (long)x;
+        for(int i = 0;i < n;i++){
+            x *= 10;
+            sb.append((int)x);
+            x -= (int)x;
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public void print(float f){
+        super.print(dtos(f, 20));
+    }
+    @Override
+    public void println(float f){
+        super.println(dtos(f, 20));
+    }
+    @Override
+    public void print(double d){
+        super.print(dtos(d, 20));
+    }
+    @Override
+    public void println(double d){
+        super.println(dtos(d, 20));
+    }
+    
+    
+
+    public void printArray(int[] array, String separator){
+        int n = array.length;
+        if(n==0){
+            super.println();
+            return;
+        }
+        for(int i=0; i<n-1; i++){
+            super.print(array[i]);
+            super.print(separator);
+        }
+        super.println(array[n-1]);
+    }
+    public void printArray(int[] array){
+        this.printArray(array, " ");
+    }
+    public void printArray(int[] array, String separator, java.util.function.IntUnaryOperator map){
+        int n = array.length;
+        if(n==0){
+            super.println();
+            return;
+        }
+        for(int i=0; i<n-1; i++){
+            super.print(map.applyAsInt(array[i]));
+            super.print(separator);
+        }
+        super.println(map.applyAsInt(array[n-1]));
+    }
+    public void printArray(int[] array, java.util.function.IntUnaryOperator map){
+        this.printArray(array, " ", map);
+    }
+
+    public void printArray(long[] array, String separator){
+        int n = array.length;
+        if(n==0){
+            super.println();
+            return;
+        }
+        for(int i=0; i<n-1; i++){
+            super.print(array[i]);
+            super.print(separator);
+        }
+        super.println(array[n-1]);
+    }
+    public void printArray(long[] array){
+        this.printArray(array, " ");
+    }
+    public void printArray(long[] array, String separator, java.util.function.LongUnaryOperator map){
+        int n = array.length;
+        if(n==0){
+            super.println();
+            return;
+        }
+        for(int i=0; i<n-1; i++){
+            super.print(map.applyAsLong(array[i]));
+            super.print(separator);
+        }
+        super.println(map.applyAsLong(array[n-1]));
+    }
+    public void printArray(long[] array, java.util.function.LongUnaryOperator map){
+        this.printArray(array, " ", map);
+    }    
+}
