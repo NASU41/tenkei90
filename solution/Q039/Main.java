03ad8613b798cd55b0917c9d5a9d7d1a0beb3fc3
NASU41 <sakon.n68@gmail.com>
2021-06-18 20:41:19 +0900

    solved Q039

---
+++ solution/Q039/Main.java
@@ -0,0 +1,590 @@
+import java.util.*;
+
+public class Main {
+
+    private static int[] subtreeSize(UndirectedGraph graph, int root){
+        int[] ans = new int[graph.V];
+        Arrays.fill(ans, -2);
+        subtreeSize(graph, root, ans);
+        return ans;
+    }
+    private static int subtreeSize(UndirectedGraph graph, int cur, int[] ans){
+        if(ans[cur] >= 0) return ans[cur];
+        ans[cur] = -1;
+        int a = 1;
+        for(UndirectedGraph.Edge e: graph.getEdges(cur)){
+            int v = e.getOtherVertex(cur);
+            if(ans[v] == -2) a += subtreeSize(graph, v, ans);
+        }
+        ans[cur] = a;
+        return a;
+    }
+    public static void main(String[] args){
+        ContestScanner sc = new ContestScanner();
+        ContestPrinter pr = new ContestPrinter();
+
+        final int N = sc.nextInt();
+        UndirectedGraph G = new UndirectedGraph(N);
+        for(int e=0; e<N-1; e++){
+            G.addEdge(sc.nextInt()-1, sc.nextInt()-1);
+        }
+
+        int[] sub = subtreeSize(G, 0);
+        //pr.println(Arrays.toString(sub));
+
+        long ans = 0;
+        for(int v=1; v<N; v++){
+            ans += 1L * sub[v] * (N - sub[v]);
+        }
+        pr.println(ans);
+
+        pr.close();
+    }
+}
+
+class UndirectedGraph{
+    private static long INF = 1L << 60;
+    class Edge implements Comparable<Edge>{
+        int u,v;
+        long length;
+        public Edge(int u, int v, long l){
+            this.u = Math.min(u, v);
+            this.v = Math.max(u, v);
+            this.length = l;
+        }
+        public long getLength(){return length;}
+        public int getLeftVertex(){return u;}
+        public int getRightVertex(){return v;}
+        public int getOtherVertex(int x){
+            return u+v-x;
+        }
+        public String toString(){
+            return String.format("[%d<->%d, %d]", u, v, length);
+        }
+        public int compareTo(Edge o){
+            return java.util.Comparator.comparingLong(Edge::getLength)
+                   .thenComparingInt(Edge::getLeftVertex)
+                   .thenComparingInt(Edge::getRightVertex)
+                   .compare(this, o);
+        }
+    }
+
+    int V, E;
+    java.util.List<Edge>[] edge;
+    java.util.ArrayList<Edge> allEdge;
+
+    public UndirectedGraph(int V){
+        this.V = V;
+        this.E = 0;
+
+        allEdge = new java.util.ArrayList<>();
+        edge = new java.util.ArrayList[V];
+        for(int v=0; v<V; v++) edge[v] = new java.util.ArrayList<>();
+    }
+    public void addEdge(int u, int v, long length){
+        E++;
+        Edge e = new Edge(u, v, length);
+        allEdge.add(e);
+        edge[u].add(e);
+        edge[v].add(e);
+    }
+    public void addEdge(int u, int v){
+        addEdge(u, v, 1L);
+    }
+
+    public java.util.List<Edge> getEdges(int v){return edge[v];}
+    public java.util.List<Edge> getAllEdges(){return allEdge;}
+
+    public long[] dijkstra(int start){
+        long[] ans = new long[V];
+        java.util.Arrays.fill(ans, INF);
+        java.util.PriorityQueue<Pair<Long,Integer>> queue = new java.util.PriorityQueue<>();
+        queue.add(new Pair<>(0L, start));
+
+        while(!queue.isEmpty()){
+            Pair<Long,Integer> top = queue.poll();
+            long d = top.getFirst();
+            int v = top.getSecond();
+            
+            if(d > ans[v]) continue;
+
+            ans[v] = d;
+            for(Edge e: edge[v]){
+                if(d + e.getLength() < ans[e.getOtherVertex(v)]){
+                    queue.add(new Pair<>(d + e.getLength(), e.getOtherVertex(v)));
+                }                
+            }
+        }
+        return ans;
+    }
+    public long[] bellmanford(int start) throws Exception{
+        long[] distance = new long[V];
+        java.util.Arrays.fill(distance, INF);
+        distance[start] = 0;
+        for(int i=0; i<V; i++){
+            for(Edge e: allEdge){
+                int u = e.getLeftVertex(), v = e.getRightVertex();
+                long l = e.getLength();
+                if(distance[v] > distance[u] + l) distance[v] = distance[u] + l;
+            }
+        }
+
+        for(Edge e: allEdge){
+            int u = e.getLeftVertex(), v = e.getRightVertex();
+            long l = e.getLength();
+            if(distance[v] > distance[u] + l) throw new Exception("Bellman-Ford algorithm: negative-weight cycle found");
+        }
+
+        return distance;
+    }
+    public long[][] warshallfloyd(){
+        long[][] ans = new long[V][V];
+        for(int i=0; i<V; i++)for(int j=0; j<V; j++){
+            ans[i][j] = INF;
+        }
+        for(Edge e: allEdge){
+            ans[e.getLeftVertex()][e.getRightVertex()] = e.getLength();
+            ans[e.getRightVertex()][e.getLeftVertex()] = e.getLength();
+        }
+
+        for(int k=0; k<V; k++)for(int i=0; i<V; i++)for(int j=0; j<V; j++){
+            ans[i][j] = Math.min(ans[i][j], ans[i][k]+ans[k][j]);
+        }
+        return ans;
+    }
+
+    public long[] bfs01(int start){
+        long[] ans = new long[V];
+        java.util.Arrays.fill(ans, INF);
+        ans[start] = 0;
+ 
+        java.util.LinkedList<Integer> queue = new java.util.LinkedList<>();
+        queue.add(start);
+        while(!queue.isEmpty()){
+            int v = queue.pollFirst();
+            for(Edge e: getEdges(v)) if(ans[e.getOtherVertex(v)] > ans[v] + e.getLength()){
+                ans[e.getOtherVertex(v)] = ans[v] + e.getLength();
+                if(e.getLength() > 0) queue.addLast(e.getOtherVertex(v));
+                else queue.addFirst(e.getOtherVertex(v));
+            }
+        }
+        return ans;
+    }
+
+    public UndirectedGraph kruskal() throws Exception{
+        UndirectedGraph ans = new UndirectedGraph(V);
+
+        java.util.Collections.sort(allEdge);
+
+        DSU uf = new DSU(V);
+        for(Edge e:allEdge){
+            if(!uf.same(e.getLeftVertex(), e.getRightVertex())){
+                uf.merge(e.getLeftVertex(), e.getRightVertex());
+                ans.addEdge(e.getLeftVertex(), e.getRightVertex(), e.getLength());
+            }
+        }
+
+        if(uf.groups().size() > 1) throw new Exception("kruskal algorithm: the graph is disconnected");
+        return ans;
+    }
+
+    public String toString(){return allEdge.toString();}
+}
+
+class DSU {
+	private int n;
+	private int[] parentOrSize;
+
+	public DSU(int n) {
+		this.n = n;
+		this.parentOrSize = new int[n];
+		java.util.Arrays.fill(parentOrSize, -1);
+	}
+
+	int merge(int a, int b) {
+		if (!(0 <= a && a < n))
+			throw new IndexOutOfBoundsException("a=" + a);
+		if (!(0 <= b && b < n))
+			throw new IndexOutOfBoundsException("b=" + b);
+
+		int x = leader(a);
+		int y = leader(b);
+		if (x == y) return x;
+		if (-parentOrSize[x] < -parentOrSize[y]) {
+			int tmp = x;
+			x = y;
+			y = tmp;
+		}
+		parentOrSize[x] += parentOrSize[y];
+		parentOrSize[y] = x;
+		return x;
+	}
+
+	boolean same(int a, int b) {
+		if (!(0 <= a && a < n))
+			throw new IndexOutOfBoundsException("a=" + a);
+		if (!(0 <= b && b < n))
+			throw new IndexOutOfBoundsException("b=" + b);
+		return leader(a) == leader(b);
+	}
+
+	int leader(int a) {
+		if (parentOrSize[a] < 0) {
+			return a;
+		} else {
+			parentOrSize[a] = leader(parentOrSize[a]);
+			return parentOrSize[a];
+		}
+	}
+
+	int size(int a) {
+		if (!(0 <= a && a < n))
+			throw new IndexOutOfBoundsException("" + a);
+		return -parentOrSize[leader(a)];
+	}
+
+	java.util.ArrayList<java.util.ArrayList<Integer>> groups() {
+		int[] leaderBuf = new int[n];
+		int[] groupSize = new int[n];
+		for (int i = 0; i < n; i++) {
+			leaderBuf[i] = leader(i);
+			groupSize[leaderBuf[i]]++;
+		}
+		java.util.ArrayList<java.util.ArrayList<Integer>> result = new java.util.ArrayList<>(n);
+		for (int i = 0; i < n; i++) {
+			result.add(new java.util.ArrayList<>(groupSize[i]));
+		}
+		for (int i = 0; i < n; i++) {
+			result.get(leaderBuf[i]).add(i);
+		}
+		result.removeIf(java.util.ArrayList::isEmpty);
+		return result;
+	}
+}
+
+class Pair<S extends Comparable<S>, T extends Comparable<T>> implements Comparable<Pair<S,T>>{
+    S first;
+    T second;
+
+    public Pair(S s, T t){
+        first = s;
+        second = t;
+    }
+    public S getFirst(){return first;}
+    public T getSecond(){return second;}
+    public boolean equals(Object another){
+        if(this==another) return true;
+        if(!(another instanceof Pair)) return false;
+        Pair otherPair = (Pair)another;
+        return this.first.equals(otherPair.first) && this.second.equals(otherPair.second);
+    }
+    public int compareTo(Pair<S,T> another){
+        java.util.Comparator<Pair<S,T>> comp1 = java.util.Comparator.comparing(Pair::getFirst);
+        java.util.Comparator<Pair<S,T>> comp2 = comp1.thenComparing(Pair::getSecond);
+        return comp2.compare(this, another);
+    }
+    public int hashCode(){
+        return first.hashCode() * 10007 + second.hashCode();
+    }
+    public String toString(){
+        return String.format("(%s, %s)", first, second);
+    }
+}
+
+class ContestScanner {
+    private final java.io.InputStream in;
+    private final byte[] buffer = new byte[1024];
+    private int ptr = 0;
+    private int buflen = 0;
+
+    private static final long LONG_MAX_TENTHS = 922337203685477580L;
+    private static final int LONG_MAX_LAST_DIGIT = 7;
+    private static final int LONG_MIN_LAST_DIGIT = 8;
+
+    public ContestScanner(java.io.InputStream in){
+        this.in = in;
+    }
+    public ContestScanner(java.io.File file) throws java.io.FileNotFoundException{
+        this.in = new java.io.BufferedInputStream(new java.io.FileInputStream(file));
+    }
+    public ContestScanner(){
+        this(System.in);
+    }
+ 
+    private boolean hasNextByte() {
+        if (ptr < buflen) {
+            return true;
+        }else{
+            ptr = 0;
+            try {
+                buflen = in.read(buffer);
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
+            if (buflen <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private int readByte() { 
+        if (hasNextByte()) return buffer[ptr++]; else return -1;
+    }
+    private static boolean isPrintableChar(int c) {
+        return 33 <= c && c <= 126;
+    }
+    public boolean hasNext() {
+        while(hasNextByte() && !isPrintableChar(buffer[ptr])) ptr++;
+        return hasNextByte();
+    }
+    public String next() {
+        if (!hasNext()) throw new java.util.NoSuchElementException();
+        StringBuilder sb = new StringBuilder();
+        int b = readByte();
+        while(isPrintableChar(b)) {
+            sb.appendCodePoint(b);
+            b = readByte();
+        }
+        return sb.toString();
+    }
+ 
+    public long nextLong() {
+        if (!hasNext()) throw new java.util.NoSuchElementException();
+        long n = 0;
+        boolean minus = false;
+        int b = readByte();
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+        if (b < '0' || '9' < b) {
+            throw new NumberFormatException();
+        }
+        while (true) {
+            if ('0' <= b && b <= '9') {
+                int digit = b - '0';
+                if (n >= LONG_MAX_TENTHS) {
+                    if (n == LONG_MAX_TENTHS) {
+                        if (minus) {
+                            if (digit <= LONG_MIN_LAST_DIGIT) {
+                                n = -n * 10 - digit;
+                                b = readByte();
+                                if (!isPrintableChar(b)) {
+                                    return n;
+                                } else if (b < '0' || '9' < b) {
+                                    throw new NumberFormatException(
+                                        String.format("%d%s... is not number", n, Character.toString(b))
+                                    );
+                                }
+                            }
+                        } else {
+                            if (digit <= LONG_MAX_LAST_DIGIT) {
+                                n = n * 10 + digit;
+                                b = readByte();
+                                if (!isPrintableChar(b)) {
+                                    return n;
+                                } else if (b < '0' || '9' < b) {
+                                    throw new NumberFormatException(
+                                        String.format("%d%s... is not number", n, Character.toString(b))
+                                    );
+                                }
+                            }
+                        }
+                    }
+                    throw new ArithmeticException(
+                        String.format("%s%d%d... overflows long.", minus ? "-" : "", n, digit)
+                    );
+                }
+                n = n * 10 + digit;
+            }else if(b == -1 || !isPrintableChar(b)){
+                return minus ? -n : n;
+            }else{
+                throw new NumberFormatException();
+            }
+            b = readByte();
+        }
+    }
+    public int nextInt() {
+        long nl = nextLong();
+        if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE) throw new NumberFormatException();
+        return (int) nl;
+    }
+    public double nextDouble() {
+        return Double.parseDouble(next());
+    }
+ 
+    public long[] nextLongArray(int length){
+        long[] array = new long[length];
+        for(int i=0; i<length; i++) array[i] = this.nextLong();
+        return array;
+    }
+    public long[] nextLongArray(int length, java.util.function.LongUnaryOperator map){
+        long[] array = new long[length];
+        for(int i=0; i<length; i++) array[i] = map.applyAsLong(this.nextLong());
+        return array;
+    }
+    public int[] nextIntArray(int length){
+        int[] array = new int[length];
+        for(int i=0; i<length; i++) array[i] = this.nextInt();
+        return array;
+    }
+    public int[] nextIntArray(int length, java.util.function.IntUnaryOperator map){
+        int[] array = new int[length];
+        for(int i=0; i<length; i++) array[i] = map.applyAsInt(this.nextInt());
+        return array;
+    }
+    public double[] nextDoubleArray(int length){
+        double[] array = new double[length];
+        for(int i=0; i<length; i++) array[i] = this.nextDouble();
+        return array;
+    }
+    public double[] nextDoubleArray(int length, java.util.function.DoubleUnaryOperator map){
+        double[] array = new double[length];
+        for(int i=0; i<length; i++) array[i] = map.applyAsDouble(this.nextDouble());
+        return array;
+    }
+ 
+    public long[][] nextLongMatrix(int height, int width){
+        long[][] mat = new long[height][width];
+        for(int h=0; h<height; h++) for(int w=0; w<width; w++){
+            mat[h][w] = this.nextLong();
+        }
+        return mat;
+    }
+    public int[][] nextIntMatrix(int height, int width){
+        int[][] mat = new int[height][width];
+        for(int h=0; h<height; h++) for(int w=0; w<width; w++){
+            mat[h][w] = this.nextInt();
+        }
+        return mat;
+    }
+    public double[][] nextDoubleMatrix(int height, int width){
+        double[][] mat = new double[height][width];
+        for(int h=0; h<height; h++) for(int w=0; w<width; w++){
+            mat[h][w] = this.nextDouble();
+        }
+        return mat;
+    }
+ 
+    public char[][] nextCharMatrix(int height, int width){
+        char[][] mat = new char[height][width];
+        for(int h=0; h<height; h++){
+            String s = this.next();
+            for(int w=0; w<width; w++){
+                mat[h][w] = s.charAt(w);
+            }
+        }
+        return mat;
+    }
+}
+
+class ContestPrinter extends java.io.PrintWriter{
+    public ContestPrinter(java.io.PrintStream stream){
+        super(stream);
+    }
+    public ContestPrinter(){
+        super(System.out);
+    }
+    public ContestPrinter(java.io.File file) throws java.io.FileNotFoundException{
+        super(file);
+    }
+    
+    private static String dtos(double x, int n) {
+        StringBuilder sb = new StringBuilder();
+        if(x < 0){
+            sb.append('-');
+            x = -x;
+        }
+        x += Math.pow(10, -n)/2;
+        sb.append((long)x);
+        sb.append(".");
+        x -= (long)x;
+        for(int i = 0;i < n;i++){
+            x *= 10;
+            sb.append((int)x);
+            x -= (int)x;
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public void print(float f){
+        super.print(dtos(f, 20));
+    }
+    @Override
+    public void println(float f){
+        super.println(dtos(f, 20));
+    }
+    @Override
+    public void print(double d){
+        super.print(dtos(d, 20));
+    }
+    @Override
+    public void println(double d){
+        super.println(dtos(d, 20));
+    }
+    
+    
+
+    public void printArray(int[] array, String separator){
+        int n = array.length;
+        if(n==0){
+            super.println();
+            return;
+        }
+        for(int i=0; i<n-1; i++){
+            super.print(array[i]);
+            super.print(separator);
+        }
+        super.println(array[n-1]);
+    }
+    public void printArray(int[] array){
+        this.printArray(array, " ");
+    }
+    public void printArray(int[] array, String separator, java.util.function.IntUnaryOperator map){
+        int n = array.length;
+        if(n==0){
+            super.println();
+            return;
+        }
+        for(int i=0; i<n-1; i++){
+            super.print(map.applyAsInt(array[i]));
+            super.print(separator);
+        }
+        super.println(map.applyAsInt(array[n-1]));
+    }
+    public void printArray(int[] array, java.util.function.IntUnaryOperator map){
+        this.printArray(array, " ", map);
+    }
+
+    public void printArray(long[] array, String separator){
+        int n = array.length;
+        if(n==0){
+            super.println();
+            return;
+        }
+        for(int i=0; i<n-1; i++){
+            super.print(array[i]);
+            super.print(separator);
+        }
+        super.println(array[n-1]);
+    }
+    public void printArray(long[] array){
+        this.printArray(array, " ");
+    }
+    public void printArray(long[] array, String separator, java.util.function.LongUnaryOperator map){
+        int n = array.length;
+        if(n==0){
+            super.println();
+            return;
+        }
+        for(int i=0; i<n-1; i++){
+            super.print(map.applyAsLong(array[i]));
+            super.print(separator);
+        }
+        super.println(map.applyAsLong(array[n-1]));
+    }
+    public void printArray(long[] array, java.util.function.LongUnaryOperator map){
+        this.printArray(array, " ", map);
+    }    
+}
